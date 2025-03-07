a889e7c101151164aff2dfbbe7b344468d5b6f4b
Aniket Sangohi <as@kali-linux.ccobravo.net>
2025-03-07 19:13:57 +0530

    removed everything..........

--- assets/js/contentLoader.js
+++
@@ -1,9 +0,0 @@
-export async function loadContent(type) {
-    try {
-        const response = await fetch(`./assets/data/${type}.json`);
-        return await response.json();
-    } catch (error) {
-        console.error(`Error loading ${type}:`, error);
-        return null;
-    }
-}
